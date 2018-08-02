d3f6d59f365f7d221ac151f8d2561e66fe0d7320
Lenon Mauer <lenonmauer@gmail.com>
2018-08-02 19:04:13 -0300

    adicionado classe para manipular a sessão do usuário

---
+++ core/user-session.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+class UserSession {
+  private $sessionKey = 'user';
+
+  function checkUserLogged() {
+    if (!$this->userIsLogged()) {
+      $this->redirectToLoginPage();
+    }
+  }
+
+  function setUser($user) {
+    $_SESSION[$this->sessionKey] = $user;
+  }
+
+  function getUser() {
+    return $this->userIsLogged() ? $_SESSION[$this->sessionKey] : null;
+  }
+
+  function removeUser() {
+    unset($_SESSION[$this->sessionKey]);
+  }
+
+  private function userIsLogged() {
+    return isset($_SESSION[$this->sessionKey]) && !empty($_SESSION[$this->sessionKey]['id']);
+   }
+
+  private function redirectToLoginPage() {
+    header('Location: login.php');
+    die();
+  }
+}
