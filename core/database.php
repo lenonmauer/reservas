7b47889ffed18442605faabce799730f5c225bb6
Lenon Mauer <lenonmauer@gmail.com>
2018-08-03 01:49:08 -0300

    adicionado métodos de insert, select, count, update e delete em database.php

--- core/database.php
+++ core/database.php
@@ -1,25 +1,156 @@
 <?php
+require_once __DIR__.'/../config.php';
 
-class Database {
-  private $host;
-  private $user;
-  private $password;
-  private $database;
+class Database
+{
   private $connection;
 
-  function __construct ($host = '127.0.0.1', $user = 'root', $password = '', $database = '') {
-    $this->host = $host;
-    $this->user = $user;
-    $this->password = $password;
-    $this->database = $database;
-  }
-
-  function connect() {
-    $this->connection = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+  public function connect()
+  {
+    $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
     if (!$this->connection) {
-      echo "Erroo: " . mysqli_connect_errno() . ' - ' . mysqli_connect_error();
+      $message = mysqli_connect_errno() . ' - ' . mysqli_connect_error();
+      throw new Exception($message);
       exit;
     }
+  }
+
+  public function insert($sql, $bindParams = [])
+  {
+    if (!$this->connection) {
+      throw new Exception('Sem conexão com o banco de dados para fazer o insert');
+    }
+
+    $query = $this->connection->prepare($sql);
+
+    if (!empty($bindParams)) {
+      call_user_func_array(array($query, 'bind_param'), $this->refValues($bindParams));
+    }
+
+    $query->execute();
+
+    $result = $query->get_result();
+
+    if ($result) {
+      return $this->connection->insert_id;
+    } else {
+      throw new Exception($this->connection->error);
+    }
+  }
+
+  public function select($sql, $bindParams = [])
+  {
+    if (!$this->connection) {
+      throw new Exception('Sem conexão com o banco de dados para fazer o select');
+    }
+
+    $data = [];
+
+    $query = $this->connection->prepare($sql);
+
+    if (!empty($bindParams)) {
+      call_user_func_array(array($query, 'bind_param'), $this->refValues($bindParams));
+    }
+
+    $query->execute();
+
+    $result = $query->get_result();
+
+    if (!$result) {
+      throw new Exception($this->connection->error);
+    }
+
+    while ($row = $result->fetch_assoc()) {
+      $data[] = $row;
+    }
+
+    return $data;
+  }
+
+  public function count($sql, $bindParams = [])
+  {
+    if (!$this->connection) {
+      throw new Exception('Sem conexão com o banco de dados para fazer o select');
+    }
+
+    $data = [];
+
+    $query = $this->connection->prepare($sql);
+
+    if (!empty($bindParams)) {
+      call_user_func_array(array($query, 'bind_param'), $this->refValues($bindParams));
+    }
+
+    $query->execute();
+
+    $result = $query->get_result();
+
+    if ($result) {
+      $row = $result->fetch_row();
+      return intval($row[0]);
+    }
+    else {
+      throw new Exception($this->connection->error);
+    }
+  }
+
+  public function update($sql)
+  {
+    if (!$this->connection) {
+      throw new Exception('Sem conexão com o banco de dados para fazer o update');
+    }
+
+    $query = $this->connection->prepare($sql);
+
+    if (!empty($bindParams)) {
+      call_user_func_array(array($query, 'bind_param'), $this->refValues($bindParams));
+    }
+
+    $query->execute();
+
+    if ($query->get_result()) {
+      return true;
+    } else {
+      throw new Exception($this->connection->error);
+    }
+  }
+
+  public function delete($sql)
+  {
+    if (!$this->connection) {
+      throw new Exception('Sem conexão com o banco de dados para fazer o delete');
+    }
+    $query = $this->connection->prepare($sql);
+
+    if (!empty($bindParams)) {
+      call_user_func_array(array($query, 'bind_param'), $this->refValues($bindParams));
+    }
+
+    $query->execute();
+
+    if ($query->get_result()) {
+      return true;
+    } else {
+      throw new Exception($this->connection->error);
+    }
+  }
+
+  public function close()
+  {
+    if ($this->connection) {
+      $this->connection->close();
+    }
+  }
+
+  private function refValues($arr)
+  {
+    $refs = array();
+
+    foreach ($arr as $key => $value) {
+      $refs[$key] = &$arr[$key];
+    }
+
+    return $refs;
   }
 }
