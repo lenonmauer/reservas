de080072955a96c3b4a7c1d375655093ccafbab7
Lenon Mauer <lenonmauer@gmail.com>
2018-08-03 01:47:59 -0300

    adicionado as configurações do banco em config.php

--- config.php
+++ config.php
@@ -1,3 +1,8 @@
 <?php
 define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST']);
 define('ASSETS_URL', BASE_URL.'/assets');
+
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'reservas');
