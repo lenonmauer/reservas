c52fe9d70e680c30f5859180c80fb746b602b7c7
Lenon Mauer <lenonmauer@gmail.com>
2018-08-04 13:44:02 -0300

    adicionado parametro na function url()

--- core/helpers.php
+++ core/helpers.php
@@ -1,8 +1,15 @@
 <?php
 require_once __DIR__.'/../config.php';
 
-function url($path) {
-  return BASE_URL.'/'.$path;
+function url($path, $params = []) {
+  $url = BASE_URL.'/'.$path;
+
+  if (!empty($params)) {
+    $querystring = http_build_query($params);
+    $url.= '?'.$querystring;
+  }
+
+  return $url;
 }
 
 function assets($path) {
