8f941220d5a5021840114d8e86b738d618f17be3
Lenon Mauer <lenonmauer@gmail.com>
2018-08-04 00:30:33 -0300

    corrigido require do user_model

--- api/login.php
+++ api/login.php
@@ -2,7 +2,7 @@
 require_once __DIR__.'/../core/helpers.php';
 require_once __DIR__.'/../core/response.php';
 require_once __DIR__.'/../core/session.php';
-require_once __DIR__.'/../models/user-model.php';
+require_once __DIR__.'/../models/user_model.php';
 
 $login = @$_POST['login'];
 $senha = @$_POST['senha'];
